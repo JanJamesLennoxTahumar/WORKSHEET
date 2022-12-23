fc3c73d40eab4b1391d5d7c5063730ee1e3b4f3e
JanJamesLennoxTahumar <janjameslennox.tahumar@students.isatu.edu.ph>
2022-12-23 10:52:23 +0800

    first commit

---
+++ WORKSHEET#3_TAHUMAR.R
@@ -0,0 +1,368 @@
+#Worksheet 3A
+Using Vectors
+
+1. There is a built-in vector LETTERS contains the uppercase letters of the alphabet and letters which
+contains the lowercase letters of the alphabet. LETTERS ## [1] “A” “B” “C” “D” “E” “F” “G” “H” “I” “J”
+“K” “L” “M” “N” “O” “P” “Q” “R” “S” ## [20] “T” “U” “V” “W” “X” “Y” “Z” letters ## [1] “a” “b” “c”
+“d” “e” “f” “g” “h” “i” “j” “k” “l” “m” “n” “o” “p” “q” “r” “s” ## [20] “t” “u” “v” “w” “x” “y” “z” Based on
+the above vector LETTERS: 
+  
+a. You need to produce a vector that contains the first 11 letters.
+
+
+FIRST_LETTERS <- LETTERS[1:11]
+FIRST_LETTERS
+
+
+## [1] "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K"
+
+
+b. Produce a vector that contains the odd numbered letters.
+
+
+ODD <- LETTERS [1:26 %% 2 == 1]
+ODD
+
+
+
+## [1] "A" "C" "E" "G" "I" "K" "M" "O" "Q" "S" "U" "W" "Y"
+
+
+c. Produce a vector that contains the vowels.
+
+
+VOWEL_LETTERS <- LETTERS [c(1,5,9,15,21)]
+VOWEL_LETTERS
+
+
+## [1] "A" "E" "I" "O" "U"
+
+
+Based on the above vector letters:
+  
+d. Produce a vector that contains the last 5 lowercase letters.
+
+
+FIVE_LETTERS <- LETTERS[22:26]
+FIVE_LETTERS
+
+
+## [1] "u" "v" "w" "x" "y"
+
+
+e. Produce a vector that contains letters between 15 to 24 letters in lowercase.
+
+
+FIVE_LETTERS <- LETTERS[15:24]
+FIVE_LETTERS
+
+
+## [1] "o" "p" "q" "r" "s" "t" "u" "v" "w" "x"
+
+
+2. Create a vector with the average temperatures in April for Tuguegarao City, Manila, Iloilo City, Tacloban,
+Samal Island, and Davao City. The average temperatures in Celcius are 42, 39, 34, 34, 30, and 27 degrees.
+
+
+a. What is the R code and its result for creating a character vector for the city/town of Tuguegarao City,
+Manila, Iloilo City, Tacloban, Samal Island, and Davao City? Name the object as city. The names
+should follow the same order as in the instruction.
+
+
+city <-c("Tuguegarao City", "Manila", "Iloilo City", "Tacloban", "Samal Island", "Davao City")
+city
+
+
+## [1] "Tuguegarao City" "Manila" "Iloilo City" "Tacloban"
+## [5] "Samal Island" "Davao City"
+
+
+b. The average temperatures in Celcius are 42, 39, 34, 34, 30, and 27 degrees. Name the object as temp.
+Write the R code and its output. Numbers should also follow what is in the instruction.
+
+
+TEMP <-c(42, 39, 34, 34, 30, 27)
+TEMP
+
+
+## [1] 42 39 34 34 30 27
+
+
+c. Associate the temperature temp with the city by using names() function. What is the R code and its
+result.
+
+
+NAMES(TEMP) <- city
+TEMP
+
+
+## Tuguegarao City Manila Iloilo City Tacloban Samal Island
+## 42 39 34 34 30
+## Davao City
+## 27
+
+
+
+e. From the answer in d, what is the content of index 5 and index 6? What is its R code?
+  
+  
+  NAMES_56 <- TEMP[5:6]
+  NAMES_56
+
+
+## Samal Island Davao City
+## 30 27
+
+
+Using Matrices
+
+
+2. Create a matrix of one to eight and eleven to fourteen with four columns and three # rows.
+
+
+a. What will be the R code for the #2 question and its result?
+
+
+TABLE_1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+TABLE_1
+
+
+## [,1] [,2] [,3] [,4]
+## [1,] 1 4 7 12
+## [2,] 2 5 8 13
+## [3,] 3 6 11 14
+b. Multiply the matrix by two. What is its R code and its result?
+  
+  
+  TABLE_1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+
+
+result <- TABLE_1 * 2
+result
+
+
+## [,1] [,2] [,3] [,4]
+## [1,] 2 8 14 24
+## [2,] 4 10 16 26
+
+
+2
+## [3,] 6 12 22 28
+
+
+c. What is the content of row 2? What is its R code?
+  
+  
+  TABLE_1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+  TABLE_1
+
+
+## [,1] [,2] [,3] [,4]
+## [1,] 1 4 7 12
+## [2,] 2 5 8 13
+## [3,] 3 6 11 14
+
+
+content <- c(TABLE_1[2,1], TABLE_1[2,2], TABLE_1[2,3], TABLE_1[2,4])
+content
+
+
+## [1] 2 5 8 13
+
+
+d. What will be the R code if you want to display the column 3 and column 4 in row 1 and row 2? What
+is its output?
+  
+  
+  TABLE_1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+  TABLE_1
+
+
+## [,1] [,2] [,3] [,4]
+## [1,] 1 4 7 12
+## [2,] 2 5 8 13
+## [3,] 3 6 11 14
+
+
+content <- c(TABLE_1[1,3], TABLE_1[1,4], TABLE_1[2,3], TABLE_1[2,4])
+content
+
+
+## [1] 7 12 8 13
+
+
+e. What is the R code is you want to display only the columns in 2 and 3, row 3? What is its output?
+  TABLE_1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+  TABLE_1
+## [,1] [,2] [,3] [,4]
+## [1,] 1 4 7 12
+## [2,] 2 5 8 13
+## [3,] 3 6 11 14
+
+
+content <- c(TABLE_1[3,2], TABLE_1[3,3])
+content
+
+
+## [1] 6 11
+
+
+f. What is the R code is you want to display only the columns 4? What is its output?
+  TABLE_1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+  TABLE_1
+
+
+## [,1] [,2] [,3] [,4]
+## [1,] 1 4 7 12
+## [2,] 2 5 8 13
+## [3,] 3 6 11 14
+
+
+content <- c(TABLE_1[1,4], TABLE_1[2,4], TABLE_1[3,4])
+content
+
+
+## [1] 12 13 14
+
+
+
+g. Name the rows as isa, dalawa, tatlo and columns as uno, dos, tres, quatro for the matrix that was
+created in b.‘. What is its R code and corresponding output?
+  
+  
+  TABLE_1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+  TABLE_1
+
+
+## [,1] [,2] [,3] [,4]
+## [1,] 1 4 7 12
+## [2,] 2 5 8 13
+## [3,] 3 6 11 14
+
+
+dimnames(table1) <- list(c("isa", "dalawa", "tatlo"),c("uno", "dos", "tres", "quatro"))
+TABLE_1
+## uno dos tres quatro
+## isa 1 4 7 12
+## dalawa 2 5 8 13
+## tatlo 3 6 11 14
+
+
+h. From the original matrix you have created in a, reshape the matrix by assigning a new dimension with
+dim(). New dimensions should have 2 columns and 6 rows. What will be the R code and its output?
+  
+  
+  dim(TABLE_1) <- c(6,2)
+  TABLE_1
+
+
+## [,1] [,2]
+## [1,] 1 7
+## [2,] 2 8
+## [3,] 3 11
+## [4,] 4 12
+## [5,] 5 13
+## [6,] 6 14
+
+
+Using Arrays
+
+
+3. An array contains 1, 2, 3, 6, 7, 8, 9, 0, 3, 4, 5, 1
+a. Create an array for the above numeric values. Each values will be repeated twice # What will be the R
+code if you are to create a three-dimensional array with 4 columns and 2 rows. What will be its output?
+  
+  
+  a <- c(1, 2, 3, 6, 7, 8, 9, 0, 3, 4, 5, 1)
+  a
+
+
+## [1] 1 2 3 6 7 8 9 0 3 4 5 1
+
+
+x <- array(rep(a, 2), dim = c(2,4,3))
+x
+
+
+## , , 1
+
+
+##
+## [,1] [,2] [,3] [,4]
+## [1,] 1 3 7 9
+## [2,] 2 6 8 0
+##
+
+
+## , , 2
+##
+## [,1] [,2] [,3] [,4]
+## [1,] 3 5 1 3
+## [2,] 4 1 2 6
+
+
+4
+##
+## , , 3
+##
+## [,1] [,2] [,3] [,4]
+## [1,] 7 9 3 5
+## [2,] 8 0 4 1
+
+b. How many dimensions do your array have? # It has 3 dimensions.
+dim(arra)
+  
+c. Name the rows as lowercase letters and columns as uppercase letters starting from the A. The array
+names should be “1st-Dimensional Array”, “2nd-Dimentional Array”, and “3rd-Dimensional Array”.
+What will be the R codes and its output?
+  
+a <- c(1, 2, 3, 6, 7, 8, 9, 0, 3, 4, 5, 1)
+a
+
+## [1] 1 2 3 6 7 8 9 0 3 4 5 1
+
+x <- array(rep(a, 2), dim = c(2,4,3))
+x
+
+
+## , , 1
+##
+## [,1] [,2] [,3] [,4]
+## [1,] 1 3 7 9
+## [2,] 2 6 8 0
+##
+## , , 2
+##
+## [,1] [,2] [,3] [,4]
+## [1,] 3 5 1 3
+## [2,] 4 1 2 6
+##
+## , , 3
+##
+## [,1] [,2] [,3] [,4]
+## [1,] 7 9 3 5
+## [2,] 8 0 4 1
+
+
+dimnames(x) <- list(letters[1:2], LETTERS[1:4], c("1st-Dimensional Array", "2nd-Dimensional Array", "3rd-Dimensional Array"))
+x
+
+
+## , , 1st-Dimensional Array
+##
+## A B C D
+## a 1 3 7 9
+## b 2 6 8 0
+##
+## , , 2nd-Dimensional Array
+##
+## A B C D
+## a 3 5 1 3
+## b 4 1 2 6
+##
+## , , 3rd-Dimensional Array
+##
+## A B C D
+## a 7 9 3 5
+5
+## b 8 0 4 1
